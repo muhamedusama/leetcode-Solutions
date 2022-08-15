570edb2cebfd32a296f7093ffdb1907bb2b9cd84
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2022-08-15 19:52:13 +0200

    Time: 23 ms (16.03%), Space: 6 MB (39.89%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fibonacci (int x)
+    {
+        if (x<2)
+            return x;
+        return fibonacci(x-1)+fibonacci(x-2);
+        
+    }
+    int fib(int n) {
+        return fibonacci(n);
+    }
+};
