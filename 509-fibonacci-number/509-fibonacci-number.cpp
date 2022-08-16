20e2f3b8eb421c3a6751236a89c26320e0402f36
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2022-08-16 12:31:37 +0200

    Time: 5 ms (43.79%), Space: 5.8 MB (96.14%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,21 @@
 class Solution {
+   
 public:
-    int fibonacci (int x)
+   
+    int fibonacci (int x, int dp[])
     {
         if (x<2)
             return x;
-        return fibonacci(x-1)+fibonacci(x-2);
+        if (dp[x]!=-1 &&x!=1 && x!=0)
+            return dp[x];
+         dp[x]=fibonacci(x-1,dp)+fibonacci(x-2,dp);
+        return dp[x];
         
     }
     int fib(int n) {
-        return fibonacci(n);
+         int dp[n+1];
+        for (int i=0;i<=n;i++)
+            dp[i]=-1;
+        return fibonacci(n,dp);
     }
 };
