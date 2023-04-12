1449d4ad33a985b573308dd4e33291e82df3f752
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2023-04-13 01:19:09 +0200

    Time: 43 ms (43.03%), Space: 20.9 MB (30.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -6,7 +6,7 @@ for(int i=0;i<strs.size(); i++)
 {
       string x = strs[i];
       sort(x.begin(),x.end());
-      mp [x].push_back(strs[i]);
+      mp[x].push_back(strs[i]);
 }
         vector<vector<string>>ans;
         for(auto it:mp)
