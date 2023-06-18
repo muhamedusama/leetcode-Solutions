218edac5d01ed26b338f534a7b495d4412bbdea5
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2023-06-19 01:05:24 +0300

    Time: 3 ms (91.59%), Space: 7.5 MB (45.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string palindrome;
-        stack<int>st;
         for (int i=0;i<s.length();i++)
         {
-            //cout<<s[i]<<endl;
             if (s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z' || s[i]>='0' && s[i]<='9')
             { 
-                //cout<<s[i]<<endl;
                 palindrome+=tolower(s[i]);
-               // cout<<palindrome;
-                st.push(tolower(s[i]));
             }
-             
+               
         }
-        //cout<<palindrome;
+        int j=palindrome.length()-1;
         for (int i=0;i<palindrome.length();i++)
         {
-            if (palindrome[i] != st.top())
+            if (palindrome[i] != palindrome[j])
                     return false;
-            st.pop();
+            j--;
         }
         return true;
     }
