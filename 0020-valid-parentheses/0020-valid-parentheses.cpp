0ae174f76017f19cf4bd5d0bc354133224e1fd44
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2023-06-19 18:14:08 +0300

    Time: 10 ms (7.02%), Space: 6.3 MB (79.05%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+          stack <char> x;
+        for (int i=0;i<s.length();i++)
+        {
+           if (x.empty())
+           {
+               x.push(s[i]);
+               continue;
+           }
+            
+           if (s[i]-1==x.top() || s[i]-2==x.top())
+           {cout<<x.top()<<endl;
+            x.pop();
+            continue;
+           }
+            x.push(s[i]);
+        }
+        if (x.empty())
+            return true;
+        else
+            return false;
+    }
+};
