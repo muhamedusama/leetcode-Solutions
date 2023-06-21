8f11abaa7e3dd0df64fa65729c2204904205fdcc
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2023-06-21 21:45:54 +0300

    Time: 116 ms (5.44%), Space: 15.6 MB (32.45%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int arr[200100]={0};
+         vector<int>twoindex;
+        int arr[20100]={0};
         for (int i=0;i<numbers.size();i++)
         {
+          
             arr[numbers[i]+1000]++;
         }
-        
-        vector<int>val;
         for (int i=0;i<numbers.size();i++)
         {
+            
             int temp=target-numbers[i];
-            if (arr[temp+1000]>1 && temp==numbers[i])
-            {
-                val.push_back(i+1);
-            }
-              if (arr[temp+1000]>0 && temp!=numbers[i])
-            {
-                val.push_back(i+1);
-            }
+            cout<<arr[temp+1000]<<endl;
+            if(arr[temp+1000]>1 && temp==numbers[i])
+                twoindex.push_back(i+1);
+            if(arr[temp+1000]>0 && temp!=numbers[i])
+                twoindex.push_back(i+1);
         }
-        
-        return val;
+        return twoindex;
     }
 };
