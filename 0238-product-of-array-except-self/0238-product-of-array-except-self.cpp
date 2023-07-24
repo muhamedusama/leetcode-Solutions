85b4a4d6e27fc27e26526185ecf9ac3f5ac9a3e7
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2023-07-24 06:04:44 +0300

    Time: 41 ms (6.32%), Space: 25 MB (24.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int multresult=1;
-        bool flag=false;
-        int noofzeros=0;
         int n=nums.size();
-        for (int i=0;i<n;i++)
+        vector<int>prefix(n);
+        prefix[0]=1;
+        for (int i=1;i<n;i++)
         {
-            if (nums[i]==0)
-            {
-                flag=true;
-                noofzeros++;
-            }
-            else
-            {
-               multresult*=nums[i]; 
-            }
+            prefix[i]=prefix[i-1]*nums[i-1];
         }
-        //cout<<multresult<<endl;
-        vector<int>ans;
-        for (int i=0;i<n;i++)
+        vector<int>suffix(n);
+        suffix[n-1]=1;
+        for (int i=n-2;i>=0;i--)
         {
-            if (noofzeros>1)
-            {
-                ans.push_back(0);
-                continue;   
-            }
-            if (flag==true && nums[i]!=0)
-                ans.push_back(0);
-            else if (flag==true && nums[i]==0)
-                ans.push_back(multresult);
-            else 
-            {
-                ans.push_back(multresult/nums[i]);
-            }
+            suffix[i]=suffix[i+1]*nums[i+1];
         }
-        return ans;
+         vector<int>output(n);
+        for (int i=0;i<n;i++)
+         {
+             output[i]=suffix[i]*prefix[i];
+         }
+        return output;
     }
 };
