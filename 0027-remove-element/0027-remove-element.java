3e3e108ad0634fa029204fbffd38f289a5c81efd
muhamedusama <64268893+muhamedusama@users.noreply.github.com>
2024-07-18 20:08:16 +0300

    Time: 3 ms (6.78%), Space: 42 MB (22.07%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+       int removeno=0;
+        for (int i=0 ; i<nums.length;i++)
+        {
+            if (nums[i]==val)
+            { 
+                removeno++;
+                nums[i]=100;
+            }
+        }
+        Arrays.sort(nums);
+        System.out.println(removeno);
+        return nums.length-removeno;
+    }
+}
